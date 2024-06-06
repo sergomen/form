3bd9fe6c29166516bc7cd440c38d75eeb0066845
Sergei Iu <serg_omen@mail.ru>
2024-06-06 23:17:35 +0300

    (fix): deleted unnecessary validationSchema

--- src/app/lib/validationSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const userValidationSchema = z.object({
-    name: z.string().min(2, 'Name is required').max(255),
-    // phone: z.string().phone('Phone is required').max(255),
-    // email: z.string().string('Invalid email').max(15),
-    // skill: z.string().skill('Invalid skill').max(255)
-});
